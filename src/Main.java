85a93edb951c964a68db233aebadb9e597952e83
Anna <annanen@inbox.ru>
2023-10-29 22:24:34 +0300

    Tasks 1 - 4 completed

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,68 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        tasks();
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] accountingBook = new int[30];
+        for (int i = 0; i < accountingBook.length; i++) {
+            accountingBook[i] = random.nextInt(100_000) + 100_000;
         }
+        return accountingBook;
+    }
+
+    public static void tasks() {
+        int[] accountingBook = generateRandomArray();
+
+        //TASK_1
+        {
+            int sum = 0;
+            System.out.println("ЗАДАЧА_1");
+            for (int current : accountingBook) {
+                sum += current;
+            }
+            System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+        }
+        //TASK_2
+        {
+            int minCurrent = accountingBook[0];
+            int maxCurrent = -1;
+            System.out.println("ЗАДАЧА_2");
+            for (int current : accountingBook) {
+                if (current > maxCurrent) {
+                    maxCurrent = current;
+                }
+                if (current < minCurrent) {
+                    minCurrent = current;
+                }
+            }
+            System.out.println("Минимальная сумма трат за день составила " + minCurrent + " рублей.");
+            System.out.println("Максимальная сумма трат за день составила " + maxCurrent + " рублей.");
+        }
+        //TASK_3
+        {
+            int sum = 0;
+            double averageValue;
+            System.out.println("ЗАДАЧА_3");
+            for (int current : accountingBook) {
+                sum += current;
+            }
+            averageValue = (double) sum / accountingBook.length;
+            System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+            System.out.println("Средняя сумма трат за месяц составила " + averageValue + " рублей");
+        }
+        //TASK_4
+        {
+            System.out.println("ЗАДАЧА_3");
+            char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+            for (int i = reverseFullName.length - 1; i >= 0; i--) {
+                System.out.print(reverseFullName[i]);
+            }
+        }
+
     }
 }
+
